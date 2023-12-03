aef233d514fec2d043e585977236011730dfbc70
Christoph Wildhagen <git@christoph-wildhagen.de>
2023-12-03 20:23:37 +0000

    Add scaffolding to src in template

--- daily-template/src/bin/part1.rs
+++ daily-template/src/bin/part1.rs
@@ -1,3 +1,17 @@
+use aoc_utils;
+
 fn main() {
-    println!("TODO")
+    let input = aoc_utils::load_input!();
+    part1(input);
+}
+
+fn part1(input: Vec<&str>) -> String {
+    todo!()
+}
+
+#[test]
+fn test_input() {
+    let test_input: Vec<&str> = "".split("\n").collect();
+    let result = part1(test_input);
+    assert_eq!(result, "".to_string())
 }
--- daily-template/src/bin/part2.rs
+++ daily-template/src/bin/part2.rs
@@ -1,3 +1,17 @@
+use aoc_utils;
+
 fn main() {
-    println!("TODO")
+    let input = aoc_utils::load_input!();
+    part2(input);
+}
+
+fn part2(input: Vec<&str>) -> String {
+    todo!()
+}
+
+#[test]
+fn test_input() {
+    let test_input: Vec<&str> = "".split("\n").collect();
+    let result = part2(test_input);
+    assert_eq!(result, "".to_string())
 }
