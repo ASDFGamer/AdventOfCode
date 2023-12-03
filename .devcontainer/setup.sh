46032074e4ea36748a3d18e00546a9fb5e191738
Christoph Wildhagen <git@christoph-wildhagen.de>
2023-12-03 19:23:13 +0000

    Install just and cargo-generate in devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cargo install cargo-generate
+#cargo install cargo-watch
+
+curr_dir=$(pwd)
+
+mkdir ~/bin
+cd ~/bin
+
+# https://github.com/casey/just#packages
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/bin
+export PATH="$PATH:$HOME/bin"
+echo 'PATH="$PATH:$HOME/bin"' >> ~/.bashrc
+
+cd $curr_dir
+echo "Devconatiner setup finished"
